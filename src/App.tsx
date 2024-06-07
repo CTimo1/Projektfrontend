71b0c8f6a12e1d225a4b944d598985e7d867f950
CTimo1 <ctimo665@gmail.com>
2024-06-07 17:38:35 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import Switch from "@mui/material/Switch";
+const label = { inputProps: { "aria-label": "Switch demo" } };
 
 function App() {
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("som tu", event.target.checked);
+    if (event.target.checked === true) {
+      console.log("ano");
+      fetch("http://192.168.50.228:3333/zapni");
+    } else {
+      console.log("nie");
+      fetch("http://192.168.50.228:3333/vypni");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Switch {...label} defaultChecked onChange={changeHandler} />
       </header>
     </div>
   );
